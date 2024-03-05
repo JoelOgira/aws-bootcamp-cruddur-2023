c6d7304ed88fd068c2370cb01b7bd2e42a0d6fb4
JoelOgira <joelogira@gmail.com>
2024-03-05 11:50:13 +0000

    Integrated aws-cognito

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -11,6 +11,24 @@ import MessageGroupPage from './pages/MessageGroupPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
 import process from 'process';
+
+import { Amplify } from 'aws-amplify'
+
+Amplify.configure({
+  "AWS_PROJECT_REGION": process.env.REACT_APP_AWS_PROJECT_REGION,
+  "aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
+  "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
+  "aws_user_pools_web_client_id": process.env.REACT_APP_CLIENT_ID,
+  "oauth": {},
+  Auth: {
+    // We are not using identity pool
+    // IdetityPoodId: 
+    region: process.env.REACT_APP_AWS_PROJECT_REGION,
+    userPoolId: process.env.REACT_APP_AWS_USER_POOLS_ID,
+    userPoolWebClientId: process.env.REACT_APP_AWS_USER_USER_POOLS_WEB_CLIENT_ID
+  }
+})
+
 import {
   createBrowserRouter,
   RouterProvider
